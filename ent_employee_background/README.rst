7f9957a293b5339079f69d02d0f99705ffb262d0
Cybrosys Technologies <risvana@cybrosys.info>
2025-06-30 17:45:32 +0530

    Jun 30: [ADD] Initial commit 'ent_employee_background'

---
+++ ent_employee_background/README.rst
@@ -0,0 +1,64 @@
+.. image:: https://img.shields.io/badge/license-OPL--1-red.svg
+    :target: https://www.odoo.com/documentation/18.0/legal/licenses.html
+    :alt: License: OPL-1
+
+Enterprise OHRMS Employee Background Verification
+=================================================
+This module helps you to the verification of employee background.
+
+Features
+========
+* The HR Manager can choose an employee and assign to a verification agent.
+* Assigned agent collect the details and send back to the manager.
+* The Agent can submit the expense and manager can approve the expense submitted.
+* The HR Manager can approve and reject the employee after verifying the result
+* Manager can send appropriate mails to the employee.
+
+Installation
+------------
+- www.odoo.com/documentation/18.0/setup/install.html
+- Install our custom addon
+
+Configuration
+=============
+- Additional configuration not required
+
+Company
+-------
+* `Cybrosys Techno Solutions <https://cybrosys.com/>`__
+
+License
+-------
+Odoo Proprietary License v1.0.
+(https://www.odoo.com/documentation/18.0/legal/licenses.html#odoo-apps)
+
+Credits
+-------
+* Developers:
+            (V18) Arjun S,
+            (V17) Sabeel B,
+            (V16) Archana V,
+            (V14) Muhammed Nafih,
+            Tintuk Tomin
+  Contact : odoo@cybrosys.com
+
+Contacts
+--------
+* Mail Contact : odoo@cybrosys.com
+* Website : https://cybrosys.com
+
+Bug Tracker
+-----------
+Bugs are tracked on GitHub Issues. In case of trouble, please check there if your issue has already been reported.
+
+Maintainer
+==========
+.. image:: https://cybrosys.com/images/logo.png
+   :target: https://cybrosys.com
+
+This module is maintained by Cybrosys Technologies.
+For support and more information, please visit https://www.cybrosys.com
+
+Further information
+===================
+HTML Description: `<static/description/index.html>`__
---
+++ ent_employee_background/__manifest__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+################################################################################
+#
+#    A part of OpenHRMS Project <https://www.openhrms.com>
+#
+#    Copyright (C) 2025-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
+#    Author: Arjun S (odoo@cybrosys.com)
+#
+#    This program is under the terms of the Odoo Proprietary License v1.0
+#    (OPL-1)
+#    It is forbidden to publish, distribute, sublicense, or sell copies of the
+#    Software or modified copies of the Software.
+#
+#    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+#    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+#    OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+#    USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+################################################################################
+{
+    'name': 'Enterprise HRMS Employee Background Verification',
+    'version': '18.0.1.0.0',
+    'category': 'Human Resources',
+    'summary': """Verify the background details of an Employee """,
+    'description': 'Manage the employees background verification Process '
+                   'employee verification ',
+    'author': 'Cybrosys Techno Solutions, Open HRMS',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': "https://www.cybrosys.com",
+    'depends': ['hr_recruitment', 'mail',
+                'ent_hr_employee_updation', 'contacts', 'portal', 'website'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/mail_template_data.xml',
+        'views/employee_verification_views.xml',
+        'views/res_partner_views.xml',
+        'views/agent_portal_templates.xml',
+    ],
+    'demo': ['demo/ent_employee_background_demo.xml'],
+    'images': ['static/description/banner.jpg'],
+    'license': 'OPL-1',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
