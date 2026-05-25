9767e3928ad51893ca5a18a5087336016600a4d5
Vincent Luba <vincent@biz-4-africa.com>
2026-05-25 11:51:34 +0200

    Display button reset to draft.

--- ent_loan_accounting/__manifest__.py
+++ ent_loan_accounting/__manifest__.py
@@ -22,7 +22,7 @@
 ################################################################################
 {
     'name': 'Enterprise OpenHRMS Loan Accounting',
-    'version': '1.0.4',
+    'version': '1.0.5',
     'category': 'Generic Modules/Human Resources',
     'summary': 'Open HRMS Loan Accounting',
     'description': """Manage Loan Request of Employees.Double Layer Approval 
